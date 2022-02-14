7adc97dffec65f9d1471ab163e232d14913d395b
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2022-02-14 21:27:38 +0000

    feat: create pick up object state machine pkg

---
+++ pick_up_object/src/pick_up_object/pickup_sm.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import rospy
+import smach
+
+
+# TODO: pick up state machine
+
+
+if __name__ == '__main__':
+    rospy.init_node('pick_and_pack_sm', anonymous=True)
+
+    rospy.spin()
