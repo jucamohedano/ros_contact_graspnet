fc344c929d40183711fab4261d0cad4c125d80b5
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2022-02-14 21:48:34 +0000

    feat: add simple pick up state machine node

--- pick_up_object/src/pick_up_object/pickup_sm.py
+++ pick_up_object/src/pick_up_object/pickup_sm.py
@@ -7,6 +7,30 @@ import smach
 
 
 if __name__ == '__main__':
-    rospy.init_node('pick_and_pack_sm', anonymous=True)
+    rospy.init_node('pick_up_sm', anonymous=True)
+
+    sm = smach.StateMachine(outcomes=['succeeded','failed', 'end'])
+
+    smach.StateMachine.add('Pickup', PickObject(planning_scene, arm_torso_controller), 
+                            transitions={
+                                'succeeded': 'Dropoff',
+                                'failed': 'Recovery'}, 
+                            remapping={})
+    smach.StateMachine.add('Dropoff', GoToDropOff(), 
+                            transitions={
+                                'succeeded': 'Finish',
+                                'failed': 'Finish'}, 
+                            remapping={})
+    smach.StateMachine.add('Recovery', Recovery(),
+                            transitions={
+                                'succeeded' : 'Finish',
+                                'failed': 'Finish'},
+                            remapping={})
+    smach.StateMachine.add('Finish', Recovery(),
+                            transitions={
+                                'end' : 'end'},
+                            remapping={})
+    
+    sm.set_initial_state(['Pickup'], userdata=smach.UserData())
 
     rospy.spin()
