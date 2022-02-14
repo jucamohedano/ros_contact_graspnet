f412a1c8681839708a93aba076ffbf131d55357d
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2022-02-14 21:05:05 +0000

    fix: convert contact graspnet output grasps into tiago's gripper frame

--- contact_graspnet/src/contact_graspnet/generate_grasps_server.py
+++ contact_graspnet/src/contact_graspnet/generate_grasps_server.py
@@ -7,98 +7,82 @@ import numpy as np
 import time
 import cv2
 import rospy
-from contact_graspnet.srv import GenerateGraspsSrv
+from contact_graspnet.srv import GenerateGraspsSrv, TfTransform, TfTransformRequest
 from geometry_msgs.msg import PoseArray
 from sensor_msgs.msg import CameraInfo
 import ros_numpy
 from mlsocket import MLSocket
-
-import tensorflow.compat.v1 as tf
-tf.disable_eager_execution()
-tf.disable_v2_behavior()
-
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(BASE_DIR))
-import config_utils
-
-from contact_grasp_estimator import GraspEstimator
-
-# Create a session
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = False
-config.allow_soft_placement = True
-config.gpu_options.per_process_gpu_memory_fraction=0.25
-sess = tf.Session(config=config)
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--ckpt_dir', default='../../checkpoints/tiago_weights_0', help='Log dir [default: ../../checkpoints/scene_test_2048_bs3_hor_sigma_001]')
-parser.add_argument('--np_path', default='test_data/7.npy', help='Input data: npz/npy file with keys either "depth" & camera matrix "K" or just point cloud "pc" in meters. Optionally, a 2D "segmap"')
-parser.add_argument('--png_path', default='', help='Input data: depth map png in meters')
-parser.add_argument('--K', default=None, help='Flat Camera Matrix, pass as "[fx, 0, cx, 0, fy, cy, 0, 0 ,1]"')
-parser.add_argument('--z_range', default=[0.2,1.8], help='Z value threshold to crop the input point cloud')
-parser.add_argument('--local_regions', action='store_true', default=False, help='Crop 3D local regions around given segments.')
-parser.add_argument('--filter_grasps', action='store_true', default=False,  help='Filter grasp contacts according to segmap.')
-parser.add_argument('--skip_border_objects', action='store_true', default=False,  help='When extracting local_regions, ignore segments at depth map boundary.')
-parser.add_argument('--forward_passes', type=int, default=1,  help='Run multiple parallel forward passes to mesh_utils more potential contact points.')
-parser.add_argument('--segmap_id', type=int, default=0,  help='Only return grasps of the given object id')
-parser.add_argument('--arg_configs', nargs="*", type=str, default=[], help='overwrite config parameters')
-FLAGS = parser.parse_args()
-
-global_config = config_utils.load_config(FLAGS.ckpt_dir, batch_size=FLAGS.forward_passes, arg_configs=FLAGS.arg_configs)
-
-print(str(global_config))
-print('pid: %s'%(str(os.getpid())))
-
-checkpoint_dir = FLAGS.ckpt_dir
-
-# Build the model
-grasp_estimator = GraspEstimator(global_config)
-grasp_estimator.build_network()
-
-# Add ops to save and restore all the variables.
-saver = tf.train.Saver(save_relative_paths=True)
-
-# Load weights
-print('loading weights...')
-grasp_estimator.load_weights(sess, saver, checkpoint_dir, mode='test')
-print('weights loaded!')
+import socket
+from npsocket import SocketNumpyArray
+import struct
+import pickle
+from numpySocket import NumpySocket
+from std_msgs.msg import Header
+from geometry_msgs.msg import Pose
+from visualization_msgs.msg import Marker
+from utilities.transformations import *
 
 # publisher of grasps. 200 grasps generated
-graps_publisher = rospy.Publisher('grasps_all', PoseArray, queue_size=10, latch=True)
+grasps_all_pub = rospy.Publisher('grasps_all', PoseArray, queue_size=10, latch=True)
 
 
+REMOTE_HOST = '158.176.76.55'
+PORT = 9997
 
 def generate_grasps(req):
+    frame_id = req.full_pcl.header.frame_id
 
     # reshape pcl to be in the right form to be processed
     pcl = ros_numpy.numpify(req.full_pcl)
     pcl = np.concatenate( (pcl['x'].reshape(-1,1), pcl['y'].reshape(-1,1), pcl['z'].reshape(-1,1)), axis=1) # 3x_ matrix
 
-
     # camera matrix from topic '/xtion/depth_registered/camera_info'
     K = [522.1910329546544, 0.0, 320.5, 0.0, 522.1910329546544, 240.5, 0.0, 0.0, 1.0]
 
-    
-    print('Generating Grasps...')
-    
-    HOST = '158.176.76.55'
-    PORT = 65432
-
-    # Make an ndarray
-    data = np.load('tiago_pcl.npy')
-
-    # Send data
+    grasps = []
     with MLSocket() as s:
-        s.connect((HOST, PORT)) # Connect to the port and host
-        print('sent data!')
-        r = s.send(data) # After sending the data, it will wait until it receives the reponse from the server
-        res = s.recv(1024) # waits for message
-        print(res)
+        s.connect((REMOTE_HOST, PORT)) # Connect to the port and host
+        s.sendall(pcl) # After sending the data, it will wait until it receives the reponse from the server
+        grasps = s.recv(1024)
+    # print(grasps)
+    grasps_pose_array = convert_opencv_to_tiago(frame_id, grasps)
+    grasps_all_pub.publish(grasps_pose_array)
 
+    return 1
 
+def convert_opencv_to_tiago(frame_id, grasps):
+    # change coords from NVLabs to Tiago gripper
+    pose_array = PoseArray(header=Header(frame_id=frame_id, stamp=rospy.get_rostime()))
+
+    q_rot = quaternion_from_euler(-math.pi/2., -math.pi/2., 0., 'sxyz')
+    q_rot_180 = quaternion_from_euler(math.pi, 0., 0., 'sxyz')
+
+    for grasp in grasps:
+        quat = quaternion_from_matrix(grasp)
+        quat = quaternion_multiply(quat, q_rot)
+
+        pose = Pose()
+        pose.position.x = grasp[0,3]
+        pose.position.y = grasp[1,3]
+        pose.position.z = grasp[2,3]
+
+        pose.orientation.x = quat[0]
+        pose.orientation.y = quat[1]
+        pose.orientation.z = quat[2]
+        pose.orientation.w = quat[3]
+
+        pose_180 = Pose()
+        pose_180.position = pose.position
+
+        quat = quaternion_multiply(quat, q_rot_180)
+        pose_180.orientation.x = quat[0]
+        pose_180.orientation.y = quat[1]
+        pose_180.orientation.z = quat[2]
+        pose_180.orientation.w = quat[3]
+
+        pose_array.poses.append(pose)
+        pose_array.poses.append(pose_180)
+    return pose_array
 
 
 if __name__ == '__main__':
