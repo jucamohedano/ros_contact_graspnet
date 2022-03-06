c1b4c84e8008013c7684a0a0c96f9fad0cf0e966
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2022-03-06 15:11:48 +0000

    Feat: add clear octomap function in server_clients.py

--- pick_up_object/src/pick_up_object/utils/servers_clients.py
+++ pick_up_object/src/pick_up_object/utils/servers_clients.py
@@ -1,5 +1,9 @@
 import rospy
-from contact_graspnet.srv import GenerateGraspsSrv, GenerateGraspsSrvResponse
+from contact_graspnet.srv import GenerateGraspsSrv, GenerateGraspsSrvResponse, TfTransform, TfTransformRequest
+from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
+from pick_up_object.srv import DetectObjects
+from std_srvs.srv import Empty
+import actionlib
 
 
 def detect_objs():
@@ -13,13 +17,25 @@ def detect_objs():
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
 
-def generate_grasps(full_pcl):
+def generate_grasps(full_pcl, objs_pcl):
     print('waiting for generate_grasps_server')
     rospy.wait_for_service('generate_grasps_server', timeout=10)
     try:
-        grasp_poses = rospy.ServiceProxy('generate_grasps_server', GenerateGrasps)
-        resp = grasp_poses(full_pcl)
+        grasp_poses = rospy.ServiceProxy('generate_grasps_server', GenerateGraspsSrv)
+        resp = grasp_poses(full_pcl, objs_pcl)
         print('generates grasps done!')
         return resp
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
+
+def clear_octomap():
+    print('waiting for clear_octomap')
+    rospy.wait_for_service('clear_octomap', timeout=10)
+    try:
+        clear_octo = rospy.ServiceProxy('clear_octomap', Empty)
+        resp1 = clear_octo()
+        print('clearing octomap done!')
+        return resp1
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
