a1ae5454a0000ae67edfef206dc11e9d934446fd
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2022-03-15 17:25:18 +0000

    src/pick_up_object/pickup_sm.py

--- pick_up_object/src/pick_up_object/pickup_sm.py
+++ pick_up_object/src/pick_up_object/pickup_sm.py
@@ -2,57 +2,60 @@
 import rospy
 import smach
 from pick_up_object.controllers import *
-from pick_up_object.states import GenerateGrasps, Pickup, DropOff, Recovery
-from pick_up_object.utils.planning_scene_interface import PlanningSceneInterface
+from pick_up_object.states import GenerateGrasps, Pickup, Recovery, DetectObjects
 
-# TODO: pick up state machine
-
+# TODO: update states transitions
+# TODO: dropoff state
 
 if __name__ == '__main__':
-    rospy.init_node('pick_up_sm', anonymous=True)
+    rospy.init_node('pickup_sm', anonymous=True)
 
     arm_torso_controller = ArmTorsoController()
-    planning_scene = PlanningSceneInterface()
-
+    head_controller = HeadController()
+    torso_controller = TorsoController()
 
     sm = smach.StateMachine(outcomes=['succeeded','failed', 'end'])
+    sm.userdata.prev = 'start'
 
-    smach.StateMachine.add('DetectObjects', DetectObjects(head_controller, torso_controller),
-                            transitions={
-                                'succeeded': 'GenerateGrasps',
-                                'looping': 'DetectObjects',
-                                'failed': 'Recovery'},
-                            remapping={
-                                'prev': 'prev',
-                                'objs_resp': 'objs_resp'})
-    smach.StateMachine.add('GenerateGrasps', GenerateGrasps(),
-                            transitions={
-                                'succeeded': 'Pickup',
-                                'failed': 'Recovery'}, 
-                            remapping={
-                                'grasps_resp': 'grasps_resp',
-                                'objs_resp': 'objs_resp'
-                            })
-    smach.StateMachine.add('Pickup', Pickup(planning_scene, arm_torso_controller),
-                            transitions={
-                                'succeeded': 'Dropoff',
-                                'failed': 'Recovery'}, 
-                            remapping={})
-    smach.StateMachine.add('Dropoff', DropOff(),
-                            transitions={
-                                'succeeded': 'Finish',
-                                'failed': 'Finish'}, 
-                            remapping={})
-    smach.StateMachine.add('Recovery', Recovery(),
-                            transitions={
-                                'succeeded' : 'Finish',
-                                'failed': 'Finish'},
-                            remapping={})
-    smach.StateMachine.add('Finish', Finish(),
-                            transitions={
-                                'end' : 'end'},
-                            remapping={})
-    
-    sm.set_initial_state(['GenerateGrasps'], userdata=smach.UserData())
-
+    with sm:
+        smach.StateMachine.add('DetectObjects', DetectObjects(head_controller, torso_controller, arm_torso_controller),
+                                transitions={
+                                    'succeeded': 'GenerateGrasps',
+                                    'looping': 'DetectObjects',
+                                    'failed': 'Recovery'},
+                                remapping={
+                                    'prev': 'prev',
+                                    'objs_resp': 'objs_resp'})
+        smach.StateMachine.add('GenerateGrasps', GenerateGrasps(),
+                                transitions={
+                                    'succeeded': 'Pickup',
+                                    'looping' : 'DetectObjects',
+                                    'failed': 'Recovery'}, 
+                                remapping={
+                                    'prev' : 'prev',
+                                    'objs_resp' : 'objs_resp',
+                                    'grasps_resp': 'grasps_resp'})
+        smach.StateMachine.add('Pickup', Pickup(arm_torso_controller),
+                                transitions={
+                                    'succeeded': 'end',
+                                    'failed': 'Recovery'},
+                                remapping={
+                                    'prev': 'prev',
+                                    'objs_resp': 'objs_resp',
+                                    'grasps_resp': 'grasps_resp'})
+        # smach.StateMachine.add('Dropoff', DropOff(),
+        #                         transitions={
+        #                             'succeeded': 'Finish',
+        #                             'failed': 'Finish'}, 
+        #                         remapping={})
+        smach.StateMachine.add('Recovery', Recovery(),
+                                transitions={
+                                    'failed': 'end', 
+                                    'restart' : 'DetectObjects', 
+                                    'restart_object_detection' : 'DetectObjects'},
+                                remapping={
+                                    'prev' : 'prev'
+                                })
+    sm.set_initial_state(['DetectObjects'], userdata=smach.UserData())
+    sm.execute()
     rospy.spin()
