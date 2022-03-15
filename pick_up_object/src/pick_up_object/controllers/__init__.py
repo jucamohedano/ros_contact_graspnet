4c4b5d9b49c8c8e09a03eb12d4496f7243ac3263
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2022-03-15 20:45:22 +0000

    fix: import errors, missing __init__.py files

--- pick_up_object/src/pick_up_object/controllers/__init__.py
+++ pick_up_object/src/pick_up_object/controllers/__init__.py
@@ -1,7 +1,7 @@
-from pick_and_pack_task.controllers.base_controller import BaseController
-from pick_and_pack_task.controllers.head_controller import HeadController
-from pick_and_pack_task.controllers.torso_controller import TorsoController
-from pick_and_pack_task.controllers.arm_torso_controller import ArmTorsoController
-from pick_and_pack_task.controllers.gripper_controller import GripperController
+from pick_up_object.controllers.base_controller import BaseController
+from pick_up_object.controllers.head_controller import HeadController
+from pick_up_object.controllers.torso_controller import TorsoController
+from pick_up_object.controllers.arm_torso_controller import ArmTorsoController
+from pick_up_object.controllers.gripper_controller import GripperController
 
 __all__ = ['BaseController', 'HeadController', 'TorsoController', 'ArmTorsoController', 'GripperController']
