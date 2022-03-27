733431abca544e83581081c0368455d4d66b71e6
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2022-03-27 12:57:38 +0100

    Fix: close gripper for longer period of time, and specify missing gripper link to attached collision object

--- pick_up_object/src/pick_up_object/pickup_states/grasp_object.py
+++ pick_up_object/src/pick_up_object/pickup_states/grasp_object.py
@@ -21,32 +21,33 @@ class GraspObject(smach.State):
         userdata.prev = 'GraspObject'
         collision_obj = userdata.collision_obj
         
-        print('About to Close')
+        rospy.loginfo('Going to close gripper')
+
         self.arm_torso.update_planning_scene(add=True)
         rospy.sleep(2.)
-        result = self.gripper.sync_reach_to(joint1=0.01, joint2=0.01)
+        result = self.gripper.sync_reach_to(joint1=0.01, joint2=0.01, wait=120)
 
         gripper_joints = self.arm_torso.get_joint_values()
         names = self.arm_torso._joints
-        print(gripper_joints)
-        print(names)
+        # print(gripper_joints)
+        # print(names)
         # result = play_motion_action('close')
 
         rospy.sleep(1.)
 
         if not result:
-            print('Close Failed')
             result='failed'
             # co = self.add_collision_object(objs_resp.object_clouds[obj_ind])
             # home_result = play_motion_action('home')
             # self.arm_torso_controller.sync_reach_safe_joint_space()
             # self.planning_scene.remove_world_object('object')
         else:
-            print('Attaching Object')
+            rospy.loginfo("Attaching object to gripper")
             aco = AttachedCollisionObject()
+            # aco.link_name = 'gripper_left_finger_link'
             aco.link_name = self.arm_torso.move_group.get_end_effector_link()
             aco.object = collision_obj
-            aco.touch_links = ['gripper_link', 'gripper_left_finger_link', 'gripper_right_finger_link']
+            aco.touch_links = ['gripper_link', 'gripper_tool_link', 'gripper_left_finger_link', 'gripper_right_finger_link']
             self.planning_scene.attach_object(aco)
 
             rospy.sleep(2.)
